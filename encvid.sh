a8fd8c0fb6ad8611faa6f757c3c5290a2a2bb951
Sven-Hendrik Haase <sh@lutzhaase.com>
2011-01-24 22:23:59 +0100

    Fixing bitrate parameter.

--- encvid.sh
+++ encvid.sh
@@ -5,10 +5,10 @@
 #
 
 output="${1//.*/.mkv}"
-bitrate=5000
+bitrate=5000k
 if [[ ${2} != "" ]]; then
 	bitrate=${2}
 fi
 
-ffmpeg -i "${1}" -an -pass 1 -vcodec libx264 -vpre slow_firstpass -b "${2}" -bt "${2}" -threads 0 "${output}"
-ffmpeg -i "${1}" -pass 2 -acodec libvorbis -aq 7 -vcodec libx264 -vpre slow -b "${2}" -bt "${2}" -threads 0 "${output}"
+ffmpeg -i "${1}" -an -pass 1 -vcodec libx264 -vpre slow_firstpass -b "${bitrate}" -bt "${bitrate}" -threads 0 "${output}"
+ffmpeg -i "${1}" -pass 2 -acodec libvorbis -aq 7 -vcodec libx264 -vpre slow -b "${bitrate}" -bt "${bitrate}" -threads 0 "${output}"
