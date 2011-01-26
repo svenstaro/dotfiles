ae81a338cd94851de38b1bdebc5857fe15e7a0f4
Sven-Hendrik Haase <sh@lutzhaase.com>
2011-01-26 22:02:30 +0100

    Fixing screenshot script.

--- lescreen.sh
+++ lescreen.sh
@@ -1,10 +1,9 @@
-#/bin/bash
+#!/bin/bash
 #
 # Small script for making screenshots and uploading them.
 #
 
 archey3
-echo "Le screenshotting..."
-scrot -q 1 /tmp/scrot.png
-echo "Le uploading..."
-imgurbash /tmp/scrot.png
+sleep 0.2
+echo "Le screenshotting and uploading..."
+scrot -q 1 /tmp/scrot.png --exec 'imgurbash $f'
