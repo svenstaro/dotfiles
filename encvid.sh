d3c350b6e8e1650b29c8ba742e84c22bcf9c5109
Sven-Hendrik Haase <sh@lutzhaase.com>
2011-04-12 23:35:43 +0200

    encvid.sh: Fixing typo

--- encvid.sh
+++ encvid.sh
@@ -14,5 +14,5 @@ echo -e "\033[1;31mFirst pass\033[0m"
 ffmpeg -i "${1}" -an -sn -pass 1 -vcodec libx264 -vpre slow_firstpass -b "${bitrate}" -bt "${bitrate}" -threads 0 "${output}"
 
 echo ""
-echo -e "\033[1;31mFirst pass\033[0m"
+echo -e "\033[1;31mSecond pass\033[0m"
 ffmpeg -i "${1}" -sn -pass 2 -acodec libvorbis -aq 7 -vcodec libx264 -vpre slow -b "${bitrate}" -bt "${bitrate}" -threads 0 "${output}"
