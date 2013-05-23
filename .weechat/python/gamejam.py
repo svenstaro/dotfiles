b2d1b5fbe697645c9628cac489311e5a17251bf3
Sven-Hendrik Haase <sh@lutzhaase.com>
2013-05-23 18:29:29 +0200

    Update dotfiles

--- .weechat/python/gamejam.py
+++ .weechat/python/gamejam.py
@@ -12,6 +12,9 @@ import sys
 import os
 import datetime
 import time
+import json
+import urllib2
+
 try:
     import weechat
     import_ok = True
@@ -22,57 +25,61 @@ except:
 
 SCRIPT_NAME    = "gamejam"
 SCRIPT_AUTHOR  = "Sven-Hendrik Haase <sh@lutzhaase.com>"
-SCRIPT_VERSION = "0.2"
+SCRIPT_VERSION = "0.3"
 SCRIPT_LICENSE = "GPL"
 SCRIPT_DESC    = "Provides time to/during a game contest and other commands"
-CONFIG_FILE_NAME= "gamejam" 
+CONFIG_FILE_NAME= "gamejam"
 SCRIPT_COMMAND = "gamejam"
 
 # put this into some run-time editable data type
-CHANNELS       = ("freenode-ssl.#bacongamejam")
-RULES          = "http://www.reddit.com/r/BaconGameJam/comments/eattc/reddit_game_jam_04_rules/"
-SUBREDDIT      = "http://www.reddit.com/r/BaconGameJam/"
-TIME_OF_JAM    = datetime.datetime(2011, 3, 25, 22, 0, 0, 0, None)
-LENGTH_OF_JAM  = datetime.timedelta(0, 0, 0, 0, 0, 48, 0)
+SITE = "http://bacongamejam.org"
+SITE_INFO_URL = SITE + "/site_info"
+CURRENT_JAM_INFO_URL = SITE + "/current_jam_info"
+CHANNELS = ("freenode-ssl.#bacongamejam")
 
 def rules_cb(data, buffer, date, tags, displayed, highlight, prefix, message):
     """ Prints URL to current contest rules. """
+    site_info = json.load(urllib2.urlopen(SITE_INFO_URL))
     buffer_name = weechat.buffer_get_string(buffer, "name")
     if(buffer_name in CHANNELS):
-        weechat.command(buffer, "Rules for current contest: "+RULES)
+        weechat.command(buffer, "Rules for current contest: " + site_info["rules"])
     return weechat.WEECHAT_RC_OK
 
 def subreddit_cb(data, buffer, date, tags, displayed, highlight, prefix, message):
     """ Prints URL to subreddit. """
+    site_info = json.load(urllib2.urlopen(SITE_INFO_URL))
     buffer_name = weechat.buffer_get_string(buffer, "name")
     if(buffer_name in CHANNELS):
-        weechat.command(buffer, "Subreddit: "+SUBREDDIT)
+        weechat.command(buffer, "Subreddit: " + site_info["subreddit"])
     return weechat.WEECHAT_RC_OK
 
 def site_cb(data, buffer, date, tags, displayed, highlight, prefix, message):
     """ Prints URL to subreddit. """
     buffer_name = weechat.buffer_get_string(buffer, "name")
     if(buffer_name in CHANNELS):
-        weechat.command(buffer, "Site: http://bacongamejam.org")
+        weechat.command(buffer, "Site: " + SITE)
     return weechat.WEECHAT_RC_OK
 
 def time_cb(data, buffer, date, tags, displayed, highlight, prefix, message):
     """ Prints remaining time to jam when jam is not running and outputs 
     remaining time in case jam is running. """
+    current_jam_info = json.load(urllib2.urlopen(CURRENT_JAM_INFO_URL))
+    time_of_jam = datetime.datetime.strptime(current_jam_info["start_time"], "%Y-%m-%d %H:%M:%S")
+    length_of_jam = datetime.timedelta(hours=current_jam_info["duration"])
     now = datetime.datetime.utcnow()
     buffer_name = weechat.buffer_get_string(buffer, "name")
     if(buffer_name in CHANNELS):
-        if(now < TIME_OF_JAM): 
+        if(now < time_of_jam): 
             # contest hasn't started yet, provide time to start
-            time_to_jam = TIME_OF_JAM - now
+            time_to_jam = time_of_jam - now
             weechat.command(buffer, "Time to contest: "+str(time_to_jam))
-        if(TIME_OF_JAM < now < TIME_OF_JAM + LENGTH_OF_JAM):
+        if(time_of_jam < now < time_of_jam + length_of_jam):
             # contest has already started, provide time to end
-            time_left = (TIME_OF_JAM + LENGTH_OF_JAM) - now
+            time_left = (time_of_jam + length_of_jam) - now
             weechat.command(buffer, "Contest ends in: "+str(time_left))
-        if(now > TIME_OF_JAM + LENGTH_OF_JAM):
+        if(now > time_of_jam + length_of_jam):
             # contest has already ended, provide time to last contest
-            time_left = (TIME_OF_JAM + LENGTH_OF_JAM) - now
+            time_left = (time_of_jam + length_of_jam) - now
             time_left = -time_left
             weechat.command(buffer, "Last contest is this long ago: "+str(time_left))
     return weechat.WEECHAT_RC_OK
@@ -94,7 +101,6 @@ def gj_unload_script():
 if (import_ok and
     weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION,
         SCRIPT_LICENSE, SCRIPT_DESC, "gj_unload_script", "")):
-#    GameJamSettings = GameJamSettings()
     weechat.hook_print("", "", "!rules", 1, "rules_cb", "")
     weechat.hook_print("", "", "!subreddit", 1, "subreddit_cb", "")
     weechat.hook_print("", "", "!site", 1, "site_cb", "")
