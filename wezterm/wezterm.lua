a1705d8c9366b71711fa89dea348d1c7d1ba76f9
Sven-Hendrik Haase <svenstaro@gmail.com>
2023-02-25 15:50:46 +0100

    wezterm: Fix font scaling behavior with i3

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,6 +7,7 @@ return {
       top = '0',
       bottom = '0',
     },
+    adjust_window_size_when_changing_font_size = false,
     hide_tab_bar_if_only_one_tab = true,
     cell_width = 0.9,
     underline_position = "-1pt",
