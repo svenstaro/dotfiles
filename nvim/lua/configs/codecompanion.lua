e5bb8d1aba315a316265c600139085b1f0d829b4
Sven-Hendrik Haase <svenstaro@gmail.com>
2025-03-02 02:46:23 +0100

    nvim: Better codecompanion diff

--- nvim/lua/configs/codecompanion.lua
+++ nvim/lua/configs/codecompanion.lua
@@ -42,12 +42,12 @@ return {
       show_settings = true,
       -- start_in_insert_mode = true,
     },
-    -- diff = {
-    --   enabled = true,
-    --   close_chat_at = 240, -- Close an open chat buffer if the total columns of your display are less than...
-    --   layout = "vertical", -- vertical|horizontal split for default provider
-    --   opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
-    --   provider = "mini_diff", -- default|mini_diff
-    -- },
+    diff = {
+      enabled = true,
+      close_chat_at = 240, -- Close an open chat buffer if the total columns of your display are less than...
+      layout = "vertical", -- vertical|horizontal split for default provider
+      opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
+      provider = "mini_diff", -- default|mini_diff
+    },
   },
 }
