9b92a1faa7cb4e8fc5eebf9ec65f599c35d4c7e5
Sven-Hendrik Haase <svenstaro@gmail.com>
2025-03-20 15:32:28 +0100

    nvim: Add nvim-lsp-file-operations to nvim-tree
    This uses the appropriate LSP to perform required source changes in case files are renamed.

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -3,7 +3,19 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
--- EXAMPLE
+-- nvim-lsp-file-operations config
+--
+-- Set global defaults for all servers
+lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
+  capabilities = vim.tbl_deep_extend(
+    "force",
+    vim.lsp.protocol.make_client_capabilities(),
+    -- returns configured operations if setup() was already called
+    -- or default operations if not
+    require("lsp-file-operations").default_capabilities()
+  ),
+})
+
 local servers = {
   "bashls",
   "biome",
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -26,6 +26,17 @@ return {
   },
 
   {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-tree.lua",
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
+
+  {
     -- See https://codecompanion.olimorris.dev/usage/chat-buffer/
     "olimorris/codecompanion.nvim",
     event = "VeryLazy",
