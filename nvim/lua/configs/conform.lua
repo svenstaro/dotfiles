fbae47da78c8c899fec340bb2a1f7e8bf0728222
Sven-Hendrik Haase <svenstaro@gmail.com>
2025-05-11 02:14:04 +0200

    nvim: Don't autoformat PKGBUILDs

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -7,11 +7,17 @@ local options = {
     -- html = { "prettier" },
   },
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+  format_on_save = function(bufnr)
+    -- Don't try to format PKGBUILDs. No one can format them, not even computer.
+    local ignore_filenames = { "PKGBUILD" }
+    local bufname = vim.api.nvim_buf_get_name(0)
+    local basename = vim.fn.fnamemodify(bufname, ":t")
+    if vim.tbl_contains(ignore_filenames, basename) then
+      return
+    end
+
+    return { timeout_ms = 500, lsp_fallback = true }
+  end,
 }
 
 return options
