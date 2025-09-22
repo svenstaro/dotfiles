79017912f1ca6aed1c30dfbbbf6281d0f0fd5bb8
Sven-Hendrik Haase <svenstaro@gmail.com>
2025-09-22 07:30:36 +0200

    nvim: Migrate to built-in lspconfig

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,21 +1,6 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
--- nvim-lsp-file-operations config
---
--- Set global defaults for all servers
-lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
-  capabilities = vim.tbl_deep_extend(
-    "force",
-    vim.lsp.protocol.make_client_capabilities(),
-    -- returns configured operations if setup() was already called
-    -- or default operations if not
-    require("lsp-file-operations").default_capabilities()
-  ),
-})
-
 local servers = {
   "bashls",
   "biome",
@@ -28,20 +13,4 @@ local servers = {
   "tinymist",
   "ts_ls",
 }
-local nvlsp = require "nvchad.configs.lspconfig"
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
-
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+vim.lsp.enable(servers)
