1bc6c443b713a14b6aff400b261024af86c739cc
Sven-Hendrik Haase <svenstaro@gmail.com>
2025-11-10 04:26:00 +0100

    Add some IOMMU scripts

---
+++ bin/usb-groups.sh
@@ -0,0 +1,1 @@
+for usb_ctrl in /sys/bus/pci/devices/*/usb*; do pci_path=${usb_ctrl%/*}; iommu_group=$(readlink $pci_path/iommu_group); echo "Bus $(cat $usb_ctrl/busnum) --> ${pci_path##*/} (IOMMU group ${iommu_group##*/})"; lsusb -s ${usb_ctrl#*/usb}:; echo; done
